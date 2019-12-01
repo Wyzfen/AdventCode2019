bd628769ea1427324fab2c2f201e593a50bfa45d
Wyzfen <anonuser42@hotmail.com>
2019-12-01 07:02:58 +0000

    Day1

---
+++ AdventCode2019/Day1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventCode2019
+{
+    [TestClass]
+    public class Day1
+    {
+        IEnumerable<int> values = Utils.IntsFromFile("day1.txt");
+
+        [TestMethod]
+        public void Problem1()
+        {
+            int result = values.Select(v => Calc(v)).Sum();
+
+            Assert.AreEqual(result, 3219099);
+        }
+
+        [TestMethod]
+        public void Problem2()
+        {
+            int result = values.Select(v => CalcRecurse(v)).Sum();
+
+            Assert.AreEqual(result, 4825810);
+        }
+
+        private int Calc(int value) => value / 3 - 2;
+
+        private int CalcRecurse(int value)
+        {
+            int total = 0;
+
+            while ((value = Calc(value)) > 0)
+            {
+                total += value;
+            } 
+            
+            return total;
+        }
+    }
+}
---
+++ AdventCode2019/Utils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventCode2019
+{
+    public static class Utils
+    {
+        public static IEnumerable<int> IntsFromFile(string filename) => 
+            File.ReadAllLines(filename, Encoding.UTF8).Select(s => int.Parse(s));
+
+        public static IEnumerable<int> IntsFromString(string input) =>
+            input.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None).Select(s => int.Parse(s));
+    }
+}
