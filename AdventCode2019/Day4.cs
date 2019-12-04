c503db45ef86c7ab226e4d8a2229f5dfe017bfe4
Wyzfen <anonuser42@hotmail.com>
2019-12-04 07:31:36 +0000

    Day 4

---
+++ AdventCode2019/Day4.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventCode2019
+{
+    [TestClass]
+    public class Day4
+    {
+        static int start = 264793;
+        static int end = 803935;
+        IEnumerable<string> values = Enumerable.Range(start, end - start).Select(v => v.ToString());
+
+        [TestMethod]
+        public void Problem1()
+        {
+            long result = values.Count(v => IsValidPassword(v));
+
+            Assert.AreEqual(result, 966);
+        }
+
+        [TestMethod]
+        public void Problem2()
+        {
+            long result = values.Count(v => IsValidPasswordB(v));
+
+            Assert.AreEqual(result, 628);
+        }
+
+        private bool IsValidPassword(String input) => String.Concat(input.OrderBy(c => c)) == input && // Is ordered
+                input.GroupBy(c => c).Any(g => g.Count() > 1);    // Has at least 2 same
+
+        private bool IsValidPasswordB(String input) => String.Concat(input.OrderBy(c => c)) == input && // Is ordered
+                input.GroupBy(c => c).Any(g => g.Count() == 2);   // Has set of exactly two same
+    }
+}
